993d071a1c4c890a1ef3fbf9914e0b137620ab15
Kaleb Olson <kalebroyolson@gmail.com>
2021-01-15 15:16:42 -0600

    Adds a Resource Controller (GreetingController.java)

---
+++ src/main/java/com/example/rackoribs/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.rackoribs;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private static final String template1 = "Yello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/greeting1")
+    public Greeting greeting1(@RequestParam(value = "name", defaultValue = "squirrel")String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template1, name));
+    }
+}
