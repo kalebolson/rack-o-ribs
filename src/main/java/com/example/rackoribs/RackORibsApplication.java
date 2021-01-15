4c1f557b552ac1237e0557fa38202aa965ba7757
Kaleb Olson <kalebroyolson@gmail.com>
2021-01-15 13:31:58 -0600

    Initial project structure using gradle and spring initializr Also includes a resource representation class "Greeting.java"

---
+++ src/main/java/com/example/rackoribs/RackORibsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rackoribs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RackORibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RackORibsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rackoribs/RackORibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rackoribs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RackORibsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
